2db0c31e1e22efd22ec2c04b1cfc53e187a6cebf
Tiago Campos <droiddzn68@gmail.com>
2026-05-24 00:27:41 -0300

    Criando html

--- template-react-main/react-vite/shadcn/frontend/src/App.jsx
+++ template-react-main/react-vite/shadcn/frontend/src/App.jsx
@@ -4,7 +4,13 @@ function App() {
 
   return (
     <>
-      Teste
+      <div className="flex  justify-center items-center h-screen bg-gray-950">
+        <div className="flex flex-col justify-center items-center rounded w-100 bg-gray-100">
+          <h1>Calculadora imc</h1>
+          <input type="text" placeholder="Digite seu peso" className="border border-black rounded" />
+
+        </div>
+      </div>
     </>
   )
 }
